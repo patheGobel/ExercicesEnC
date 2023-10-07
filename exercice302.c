80ddca7f7abf7f7d8094a272d98b8cb5ed3fcbe6
Pathé BA <pathegba@gmail.com>
2023-10-07 17:31:07 +0000

    Jai changer les int en double pour ne pas perdre trop d'information puis j'ai ajoutter la fonction variance() mais je dois dire aussi que j'ai eu du mal a compiler mon prgramme car je devrai faire: gcc -o exercice302 exercice302.c -lm

--- exercice302.c
+++ exercice302.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define max 100
 
 void affiche(int *arr, int taille);
-int moyenne(int *arr, int taille);
-int variance(int *arr, int taille);
+double moyenne(int *arr, int taille);
+double variance(int *arr, int taille);
+double ecartype(int *arr, int taille);
 
 int main()
 {
@@ -18,7 +20,7 @@ int main()
         scanf("%d", &T[i]);
     }
 
-    printf("Appuie sur:\n 0 pour afficher\n 1 pour la moyenne des nombres\n 2 pour la variance\n");
+    printf("Appuie sur:\n 0 pour afficher\n 1 pour la moyenne des nombres\n 2 pour la variance\n 3 pour l'écart-type\n");
     scanf("%d", &choix);
     switch (choix)
     {
@@ -26,10 +28,13 @@ int main()
         affiche(&T[0], taille);
         break;
     case 1:
-        printf("La moyenne est:%d\n", moyenne(&T[0], taille));
+        printf("La moyenne est:%f\n", moyenne(&T[0], taille));
         break;
     case 2:
-        printf("La variance est:%d\n", variance(&T[0], taille));
+        printf("La variance est:%f\n", variance(&T[0], taille));
+        break;
+    case 3:
+        printf("La moyenne est:%f\n", ecartype(&T[0], taille));
         break;
         // default:
         //   printf("La moyenne est:%d", variance(&T[0], taille));
@@ -47,25 +52,31 @@ void affiche(int *arr, int taille)
         printf("arr[%d]=%d\n", i, arr[i]);
     }
 }
-
-int moyenne(int *arr, int taille)
+//Ajout de la fonction  moyenne()
+double moyenne(int *arr, int taille)
 {
-    int sum = 0;
+    double sum = 0;
     for (int i = 0; i < taille; i++)
     {
         sum = sum + arr[i];
     }
     return (sum / taille);
 }
-
-int variance(int *arr, int taille)
+// Ajout de la fonction Variance()
+double variance(int *arr, int taille)
 {
-    int var = 0, moy = 0, sum = 0;
+    double var = 0, moy = 0, sum = 0;
     moy = moyenne(&arr[0], taille);
     for (int i = 0; i < taille; i++)
     {
         sum = sum + ((arr[i] - moy) * (arr[i] - moy));
     }
-    var = sum / (taille - 1);
+    var = sum / (taille);
     return var;
+}
+// Ajout de la fonction écart-type()
+double ecartype(int *arr, int taille)
+{
+    double ecartType = variance(&arr[0], taille);
+    return sqrt(ecartType);
 }
